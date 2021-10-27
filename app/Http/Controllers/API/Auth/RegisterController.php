a615139c91e1778936dd7744e867e47da54a6b11
AndiWawanK <andifirwansyah250@gmail.com>
2021-10-27 15:54:41 +0700

    register

---
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+use DB;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function index(Request $request){
+        $validate = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users|email',
+            'password' => 'required'
+        ]);
+
+        if($validate->fails()){
+            return response()->json($validate->errors());
+        }
+
+        DB::beginTransaction();
+        try{
+            $user = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password'))
+            ]);
+            DB::commit();
+            $token = $user->createToken($request->input('email'))->plainTextToken;
+            return response()->json([
+                'data' => $user,
+                'access_token' => $token
+            ], 201);
+        }catch(Exception $e){
+            DB::rollback();
+            return response()->json(['error' => $e]);
+        }
+    }
+}
